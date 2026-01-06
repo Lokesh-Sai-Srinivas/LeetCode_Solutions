ea7a093dab19950b65a606b049b2dd90b9ce92e5
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-01-06 16:09:01 +0530

    Create solution.java

---
+++ incoming/solutions/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        if (root.left == null && root.right == null)
+            return 1;
+        long maxSum = Long.MIN_VALUE;
+        int ans = 1;
+        int currLevel = 1;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            long currSum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                currSum += node.val;
+                if (node.left != null)
+                    q.add(node.left);
+                if (node.right != null)
+                    q.add(node.right);
+            }
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                ans = currLevel;
+            }
+            currLevel++;
+        }
+        return ans;
+    }
+}
