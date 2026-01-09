3b590fbd0a2b136af86ce957b4eb24fb919e2964
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-01-09 23:41:22 +0530

    Create solution.cpp

---
+++ incoming/solutions/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int, TreeNode*> dfs(TreeNode* node) {
+        if (!node)
+            return {-1, nullptr};
+        auto L = dfs(node->left), R = dfs(node->right);
+        if (L.first == R.first)
+            return {L.first + 1, node};
+        return (L.first > R.first) ? make_pair(L.first + 1, L.second)
+                                   : make_pair(R.first + 1, R.second);
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) { return dfs(root).second; }
+};
