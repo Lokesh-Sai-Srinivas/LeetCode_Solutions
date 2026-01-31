459dde38bbe0b339c57198f2f470dbcaa04a8086
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-01-31 19:18:29 +0530

    Create solution.cpp

---
+++ incoming/solutions/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        if(target == 'z') return letters[0];
+
+        for(char ch: letters) {
+            if(ch > target) return ch;
+        }
+        
+        return letters[0];
+    }
+};
