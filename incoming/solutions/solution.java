c17a18412aff336f2b18a0c29eb3b9ebb15967e8
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-02-01 18:41:10 +0530

    Create solution.java

---
+++ incoming/solutions/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int first = nums[0];
+        int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < a) {
+                b = a;
+                a = nums[i];
+            } else if (nums[i] < b) {
+                b = nums[i];
+            }
+        }
+
+        return first + a + b;
+    }
+}
