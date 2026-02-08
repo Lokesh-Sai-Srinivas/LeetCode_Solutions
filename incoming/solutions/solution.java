4a6c4f81f48e8446b4ef5feaf695a5f14a5a01f3
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-02-08 22:07:47 +0530

    Create solution.java

---
+++ incoming/solutions/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return height(root) != -1;
+    }
+
+    private static int height (TreeNode root) {
+        if(root == null) return 0;
+        int hl = height(root.left);
+        int hr = height(root.right);
+        
+        if(hr == -1 || hl == -1) return -1;
+
+        return (Math.abs(hl - hr) > 1) ? -1 :1 + Math.max(hl,hr);
+    }
+}
