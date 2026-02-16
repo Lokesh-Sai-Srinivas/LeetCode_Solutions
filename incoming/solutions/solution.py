e0672c753c80ef159e5d5134612d7f1c034485ce
Lokesh-Sai-Srinivas <167162278+Lokesh-Sai-Srinivas@users.noreply.github.com>
2026-02-16 22:01:07 +0530

    Create solution.py

---
+++ incoming/solutions/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 31
+        while n != 0:
+            ret += (n & 1) << power
+            n >>= 1   
+            power -= 1
+        return ret
